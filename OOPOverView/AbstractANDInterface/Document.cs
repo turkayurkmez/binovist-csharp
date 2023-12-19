32986f50efe2047479a733bd1f84252042c5b9e6
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-12-19 12:39:57 +0300

    Abstranct And interface

--- OOPOverView/AbstractANDInterface/Document.cs
+++ OOPOverView/AbstractANDInterface/Document.cs
@@ -1,5 +1,11 @@
 ﻿namespace AbstractANDInterface
 {
+
+    public interface IPrintable
+    {
+        void Print();
+    }
+
     public abstract class Document
     {
         public void Copy(string from, string to)
@@ -14,14 +20,78 @@
 
         public abstract void Save();
         public abstract void Load();
-
-        public abstract void Print();
+        //public abstract void Print();
 
     }
 
     public class PDFDocument : Document
     {
+        public override void Load()
+        {
+            Console.WriteLine("Pdf açıldı");
+        }
 
+
+
+        public override void Save()
+        {
+            Console.WriteLine("Pdf kaydedildi");
+
+        }
+    }
+
+    public class ExcelDocument : Document, IPrintable
+    {
+        public override void Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Print()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class WordDocument : Document, IPrintable
+    {
+        public override void Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Print()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class SaveDocument
+    {
+
+        public void Save(Document document)
+        {
+            document.Save();
+        }
+
+    }
+
+    public class Printer
+    {
+        public void Print(IPrintable document)
+        {
+            document.Print();
+        }
     }
 
 
--- OOPOverView/AbstractANDInterface/Program.cs
+++ OOPOverView/AbstractANDInterface/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AbstractANDInterface;
+
 Console.WriteLine("Hello, World!");
+PDFDocument pDF = new PDFDocument();
+ExcelDocument excel = new ExcelDocument();
+
+Printer printer = new Printer();
+printer.Print(excel);
